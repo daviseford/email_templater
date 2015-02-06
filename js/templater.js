78a9e87efd5b5508eecfb5300d86f0e43fe17978
daviseford <davis.e.ford.alt@gmail.com>
2015-02-06 14:28:43 -0500

    buttons, pared down to 1 story atm

--- js/templater.js
+++ js/templater.js
@@ -1,13 +1,13 @@
 // JavaScript Document
 
 $(document).ready(function() {
+	$("#resultsContainer").hide();
     var title1 = $.trim($("#title1").val());
 	var story1 = $.trim($("#text1").val());
 	var title1URL = $.trim($("#title1URL").val());
-	var title1img = $.trim($("#title1img").val());
+	var title1IMG = $.trim($("#title1IMG").val());
 	var title1KEY = $.trim($("#title1KEY").val());
 	
-	var titlehrefFix = '<a href="'+title1URL+'" target="_blank"><span style="font-size: 18px; font-weight: bold; padding-top:10px;">'+title1+'</span></a>';
 	
 //	var title2 = $.trim($("#title2").val());
 //	var story2 = $.trim($("#text2").val());
@@ -15,19 +15,18 @@ $(document).ready(function() {
 //	var title2img = $.trim($("#title2img").val());
 //	var title2KEY = $.trim($("#title2KEY").val());
 	
-	$("#accordion").accordion();
 	
 	
 	$("#generateHTML").click(function(){
 		title1 = $.trim($("#title1").val());
 		story1 = $.trim($("#text1").val());
 		title1URL = $.trim($("#title1URL").val());
-		title1img = $.trim($("#title1img").val());
+		title1IMG = $.trim($("#title1IMG").val());
 		title1KEY = $.trim($("#title1KEY").val());
 		
 		var hrefFix = '<a href="'+title1URL+'" target="_blank">';
-		var titlehrefFix = '<a href="'+title1URL+'" target="_blank"><span style="font-size: 18px; font-weight: bold; padding-top:10px;">'+title1+'</span></a>';
-		
+		var titlehrefFix = '<a href="'+title1URL+'" target="_blank"><h2 style="line-height: 1.14em;margin-top: 0.5em;margin-bottom: 0.5em;color: #0000FF;font-family: Verdana;font-size: 16px;font-weight: bold;font-style: normal;text-transform: none;text-decoration: underline;">'+title1+'</h2></a>';
+		var imageRetrieve = '<center><img src="'+title1IMG+'" width="125" height="125" alt=""></center>'
 		console.log(titlehrefFix);
 	
 	
@@ -42,7 +41,8 @@ $(document).ready(function() {
 		img: title1img,
 		key: title1KEY,
 		titlehrefFix: titlehrefFix,
-		hrefFix: hrefFix
+		hrefFix: hrefFix,
+		imageRetrieve : imageRetrieve
   		}
 	];
 
@@ -50,6 +50,7 @@ $(document).ready(function() {
 
 	$("#resultsDiv").html(html);
 	$("#resultsTextArea").val(html);
+	$("#resultsContainer").show("drop")
 	});
 
 
