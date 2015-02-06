4aa54c06303689d89feba54d5cc54738cbcccfc8
daviseford <davis.e.ford.alt@gmail.com>
2015-02-06 16:27:06 -0500

    final commit of the day, my brain hurts
    -added basic must read?
    -updated template handling
    -link parsing?
    -it basically works at this point, just really limited in utility.
    -NOT Kelly proof, this thing will take any dumbass value you throw at
    it. Need to bring my validation over from Link Checker.

--- js/templater.js
+++ js/templater.js
@@ -1,12 +1,12 @@
 // JavaScript Document
 
 $(document).ready(function() {
-	$("#resultsContainer").hide();
     var title1 = $.trim($("#title1").val());
 	var story1 = $.trim($("#text1").val());
 	var title1URL = $.trim($("#title1URL").val());
 	var title1IMG = $.trim($("#title1IMG").val());
 	var title1KEY = $.trim($("#title1KEY").val());
+	$("#resultsContainer").hide();
 	
 	
 //	var title2 = $.trim($("#title2").val());
@@ -25,9 +25,9 @@ $(document).ready(function() {
 		title1KEY = $.trim($("#title1KEY").val());
 		
 		var hrefFix = '<a href="'+title1URL+'" target="_blank">';
-		var titlehrefFix = '<a href="'+title1URL+'" target="_blank"><h2 style="line-height: 1.14em;margin-top: 0.5em;margin-bottom: 0.5em;color: #0000FF;font-family: Verdana;font-size: 16px;font-weight: bold;font-style: normal;text-transform: none;text-decoration: underline;">'+title1+'</h2></a>';
-		var imageRetrieve = '<center><img src="'+title1IMG+'" width="125" height="125" alt=""></center>'
-		console.log(titlehrefFix);
+		//var titlehrefFix = '<a href="'+title1URL+'" target="_blank"><h2 style="line-height: 1.14em;margin-top: 0.5em;margin-bottom: 0.5em;color: #0000FF;font-family: Verdana;font-size: 16px;font-weight: bold;font-style: normal;text-transform: none;text-decoration: underline;">'+title1+'</h2></a>';
+		var imageRetrieve = '<center>'+hrefFix+'<img src="'+title1IMG+'" width="150" height="150" alt=""></a></center>';
+		console.log(hrefFix);
 	
 	
 	
@@ -38,11 +38,10 @@ $(document).ready(function() {
 		title: title1,	
     	url: title1URL,
 		story: story1,
-		img: title1img,
+		img: title1IMG,
 		key: title1KEY,
-		titlehrefFix: titlehrefFix,
-		hrefFix: hrefFix,
-		imageRetrieve : imageRetrieve
+		insertURL: hrefFix,
+		insertImage : imageRetrieve
   		}
 	];
 
