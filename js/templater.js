856211257746ba103c092e1880ad0313fccce938
daviseford <davis.e.ford.alt@gmail.com>
2015-02-09 15:07:45 -0500

    working hack

--- js/templater.js
+++ js/templater.js
@@ -42,6 +42,28 @@ $(document).ready(function() {
 		var linkedTitle1 = '<h4><a href="'+title1URL+'" target="_blank">'+title1+'</a></h4>';
 		var imageRetrieve1 = '<center>'+urlInsert1+'<img src="'+title1IMG+'" class="img_thumb" alt="Story Image"></a></center>';
 		
+		
+		
+		var storyz = {
+		story: [
+		{
+		title: title1,
+		text: title1text,
+		url: title1URL,
+		imageURL: title1IMG,
+		urlInsert: urlInsert1,
+		linkedTitle: linkedTitle1,
+		insertImage: imageRetrieve1
+		},
+		]
+		};
+		
+		
+		
+		
+		
+		
+		
 		if(additionalContentVal === true) {
 			var title2 = $.trim($("#title2").val());
 			var title2text = $.trim($("#title2text").val());
@@ -51,68 +73,47 @@ $(document).ready(function() {
 			var urlInsert2 = '<a href="'+title2URL+'" target="_blank">';
 			var linkedTitle2 = '<h4><a href="'+title2URL+'" target="_blank">'+title2+'</a></h4>';
 			var imageRetrieve2 = '<center>'+urlInsert2+'<img src="'+title2IMG+'" class="img_thumb" alt="Story Image"></a></center>';
-			console.log("Additiontional content enabled, vars set");
-		}
+			console.log("Additional content enabled, vars set");
+			
+			
+			var storyz = {
+				story: [
+				{
+				title: title1,
+				text: title1text,
+				url: title1URL,
+				imageURL: title1IMG,
+				urlInsert: urlInsert1,
+				linkedTitle: linkedTitle1,
+				insertImage: imageRetrieve1
+				},
+				{
+				title: title2,
+				text: title2text,
+				url: title2URL,
+				imageURL: title2IMG,
+				urlInsert: urlInsert2,
+				linkedTitle: linkedTitle2,
+				insertImage: imageRetrieve2
+				}
+				]
+				};			
+		};
 		
-	var myTemplate = $.templates("#emailTmpl");
-	var addDivTmpl = $.templates("#addDivTmpl");
-
-	var storyx = [
-	{
-		title: title1,
-		text: title1text,
-		url: title1URL,
-		imageURL: title1IMG,
-		urlInsert: urlInsert1,
-		linkedTitle: linkedTitle1,
-		insertImage: imageRetrieve1
 		
-	},
-	{
-		title: title2,
-		text: title2text,
-		url: title2URL,
-		imageURL: title2IMG,
-		urlInsert: urlInsert2,
-		linkedTitle: linkedTitle2,
-		insertImage: imageRetrieve2
-	}
-	];
-	
-	var storyz = {
-		story: [
-	{
-		title: title1,
-		text: title1text,
-		url: title1URL,
-		imageURL: title1IMG,
-		urlInsert: urlInsert1,
-		linkedTitle: linkedTitle1,
-		insertImage: imageRetrieve1
-		
-	},
-	{
-		title: title2,
-		text: title2text,
-		url: title2URL,
-		imageURL: title2IMG,
-		urlInsert: urlInsert2,
-		linkedTitle: linkedTitle2,
-		insertImage: imageRetrieve2
-	}
-	]
-	};
-		
-	var htmlout = addDivTmpl.render(storyx);
 	//$("#hereAdd").html(htmlout);	
 	//var html = myTemplate.render(stories);
 	
 	//addDivTmpl.render(storyx); //Render the stories and hopefully put them into the overarching template
+	var myTemplate = $.templates("#emailTmpl");
+	var addDivTmpl = $.templates("#addDivTmpl");
+
 	var html = myTemplate.render(storyz);
 
 	$("#resultsDiv").html(html); //Renders the HTML version of the email
 	$("#resultsTextArea").val(html); //Puts the raw HTML into the textbox so we can easily copy it.
 	$("#resultsContainer").show("drop"); //Shows the results once everything is ready.
+	console.log(storyz);
 	});
 
 
