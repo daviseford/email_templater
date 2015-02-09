d56618f13954592487a541463e99c0f80d54da59
daviseford <davis.e.ford.alt@gmail.com>
2015-02-09 12:07:39 -0500

    switching to sub-templates

--- js/templater.js
+++ js/templater.js
@@ -1,11 +1,6 @@
 // JavaScript Document
 
 $(document).ready(function() {
-    var title1 = $.trim($("#title1").val());
-	var story1 = $.trim($("#text1").val());
-	var title1URL = $.trim($("#title1URL").val());
-	var title1IMG = $.trim($("#title1IMG").val());
-	var title1KEY = $.trim($("#title1KEY").val());
 	$("#resultsContainer").hide();
 	
 	
@@ -19,13 +14,17 @@ $(document).ready(function() {
 	
 	$("#story1").submit(function() { 
 		event.preventDefault(); //Stops page from reloading
-		title1 = $.trim($("#title1").val());
-		story1 = $.trim($("#text1").val());
-		title1URL = $.trim($("#title1URL").val());
-		title1IMG = $.trim($("#title1IMG").val());
-		title1KEY = $.trim($("#title1KEY").val());
+		var title1 = $.trim($("#title1").val());
+		var story1 = $.trim($("#text1").val());
+		var title1URL = $.trim($("#title1URL").val());
+		var title1IMG = $.trim($("#title1IMG").val());
+		var title1KEY = $.trim($("#title1KEY").val());
+		
+		if ($('#story1 input.checkbox_check').is(':checked')) {
+		};
 		
 		var hrefFix = '<a href="'+title1URL+'" target="_blank">';
+		var linkedTitle = '<h4><a href="'+title1URL+'" target="_blank">'+title1+'</a></h4>';
 		//var titlehrefFix = '<a href="'+title1URL+'" target="_blank"><h2 style="line-height: 1.14em;margin-top: 0.5em;margin-bottom: 0.5em;color: #0000FF;font-family: Verdana;font-size: 16px;font-weight: bold;font-style: normal;text-transform: none;text-decoration: underline;">'+title1+'</h2></a>';
 		var imageRetrieve = '<center>'+hrefFix+'<img src="'+title1IMG+'" class="img_thumb" alt="Story Image"></a></center>';
 		console.log(hrefFix);
@@ -34,12 +33,11 @@ $(document).ready(function() {
 	
 	var myTemplate = $.templates("#emailTmpl");
 	var addDivTmpl = $.templates("#addDivTmpl");
-	
-	var numTmpl = [];
 
 	var stories = [
   		{
-		title: title1,	
+		title: title1,
+		linkedTitle: linkedTitle,	
     	url: title1URL,
 		story: story1,
 		img: title1IMG,
@@ -54,7 +52,7 @@ $(document).ready(function() {
 
 	$("#resultsDiv").html(html); //Renders the HTML version of the email
 	$("#resultsTextArea").val(html); //Puts the raw HTML into the textbox so we can easily copy it.
-	$("#resultsContainer").show("drop") //Shows the results once everything is ready.
+	$("#resultsContainer").show("drop"); //Shows the results once everything is ready.
 	});
 
 
