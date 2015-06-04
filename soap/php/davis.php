5ec7056ff4e06e23c02d35405cd8bede3b7ab942
Davis Ford <davis.e.ford@gmail.com>
2015-06-04 18:54:31 -0400

    connection!

--- soap/php/davis.php
+++ soap/php/davis.php
@@ -36,15 +36,22 @@ $lmapi->setCredentials($userName,$password, 'basic');
 
 echo "<h3> Current version of API at " . $wsdl_location . " is: " . $api_version . "</h3>\n";
 
+$content_Title = '';
+$content_Description = '';
+$content_HeaderTo = '';
+$content_HeaderFrom = ''; //TODO: Add to MySQL table
+$content_Text = '';
+$content_HTML = '';
+
 // Create Content
         print "Running CreateContent()\n";
         $DocPartText = array ('MimePartName' => 'text', 'Body' => 'This is text body of this content', 'Encoding' => '8bit', 'CharSetID' => 1);
         $DocPartHtml = array ('MimePartName' => 'html', 'Body' => '<HTML>This is <B>text </B> body of this <U>content</U></HTML>', 'Encoding' => '8bit', 'CharSetID' => 1);
         $DocParts = array ($DocPartText, $DocPartHtml);
-        $contentStruct = array('NativeTitle' => 'This is content native title', 'Description' => 'This content description', 'Title' => 'This is content title', 'HeaderTo' => 'group@maileater.lyris.com', 'HeaderFrom' => 'sender@maileater.lyris.com', 'DocType' => 'CONTENTv2', 'DocParts' => $DocParts, 'ListName' => 'list1');
+        $contentStruct = array('NativeTitle' => 'This is content native title', 'Description' => 'This content description', 'Title' => 'This is content title', 'HeaderTo' => 'group@maileater.lyris.com', 'HeaderFrom' => 'sender@maileater.lyris.com', 'DocType' => 'CONTENTv2', 'DocParts' => $DocParts, 'ListName' => 'alpac_master');
 
         $result = $lmapi->CreateContent($contentStruct);
-                print "CreateContent result: $result\n\n";
+                print "CreateContent result: ".var_dump($result) ;
 
                 print "Running UpdateContent()\n";
                 $contentStruct = array ('ContentID' => (int)$result, 'Description' => 'Modified description of the content', 'Title' => 'Modified content title');
