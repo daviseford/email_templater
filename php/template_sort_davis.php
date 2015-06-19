97d442892f8ce93a35418188fd0ad318c2d610a4
daviseford <davis.e.ford.alt@gmail.com>
2015-06-19 12:53:20 -0400

    template updates

--- php/template_sort_davis.php
+++ php/template_sort_davis.php
@@ -23,26 +23,6 @@ if ($conn->connect_error) {
 $sql = "SELECT * FROM Templates WHERE clientCode = '$client' AND templateCode = '$template'";
 $result = $conn->query($sql);
 
-/*
-if ($result->num_rows > 0) {
-    $row = $result->fetch_assoc();
-	if ($row["clientCode"] = $clientCode) {
-		$adArray = array (
-			"id" => $row["id"],
-			"image_url" => $row["image_url"],
-			"image_width" => $row["image_height"],
-			"image_height" => $row["image_height"],
-			"ad_url" => $row["ad_url"],
-			"alt_text" => $row["alt_text"],
-			"clientCode" => $row["clientCode"],
-			"clientName" => $row["clientName"],
-			"responsive" => $row["responsive"],
-		);
-	} else {
-    echo "0 results";
-}
-}
-*/
 if ($result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
@@ -87,18 +67,8 @@ if ($result->num_rows > 0) {
 
 					//execute post
 					$curlResult = curl_exec($ch);
-					//header("Cache-Control: public");
-					//header('Content-Type: text/html; charset=utf-8');
-					
-					
-					//header("Cache-Control: public");
-//header("Content-Description: File Transfer");
-//header('Content-Length: '.strlen($curlResult));
-//header("Content-Disposition: attachment; filename=hullo.html");
-//header("Content-Type: application/octet-stream; "); 
-//header("Content-Transfer-Encoding: binary");
 
-curl_close($ch);
+					curl_close($ch);
 					
 					$conn->close();	
 
